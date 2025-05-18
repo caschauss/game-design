d040cdee78b480b85696f52fbc989884d43ac754
firefly <christian.schauss@student.uni-luebeck.de>
2025-05-18 12:52:51 +0200

    implemented getter for expression

--- src/PartyButtonBox.tsx
+++ src/PartyButtonBox.tsx
@@ -1,16 +1,26 @@
+import React, { useState } from 'react';
 import { Button } from "@mantine/core";
+import expressions from './expressions.json'
 
 interface PartyButtonBoxProps {
     handleButtonClick: () => void;
 }
 
-export const PartyButtonBox: React.FC<PartyButtonBoxProps> = ({handleButtonClick}) => {
+export const PartyButtonBox: React.FC<PartyButtonBoxProps> = ({ handleButtonClick }) => {
+    const [expressionID, setExpressionID] = useState(0);
+
+    const getNewExpression = () => {
+        console.log(JSON.stringify(expressions[expressionID]));
+        setExpressionID(expressionID + 1);
+    }
+
     return (
-        <div style={{ width: "100%", height: "50vh"}}>
+        <div style={{ width: "100%", height: "50vh" }}>
             <Button style={{ width: "50%", height: "25vh", backgroundColor: "blue" }} onClick={handleButtonClick}>Option A</Button>
             <Button style={{ width: "50%", height: "25vh", backgroundColor: "green" }} onClick={handleButtonClick}>Option B</Button>
             <Button style={{ width: "50%", height: "25vh", backgroundColor: "yellow" }} onClick={handleButtonClick}>Option C</Button>
             <Button style={{ width: "50%", height: "25vh", backgroundColor: "pink" }} onClick={handleButtonClick}>Option D</Button>
+            <button onClick={getNewExpression}>print</button>
         </div>
     );
 }
