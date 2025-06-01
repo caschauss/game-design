a3a74b68f33bce8dced1deb8fb35453f7f1761cc
firefly <schauss@tutanota.com>
2025-06-01 12:44:37 +0200

    created API endpoint to get colour

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,11 +5,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let selectedColour = "black";
+
 // API test route
 app.post('/test', (req, res) => {
   console.log("Received req: ", req.body);
   res.json({ "message": "test" });
 });
+
+app.post('/getcolour', (req, res) => {
+  let datetime = new Date();
+  console.log("Received colour request at " + datetime);
+  res.json({"colour": selectedColour});
+})
 
 // Setup
 app.listen(3000, () => {
