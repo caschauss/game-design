1c47641fe60b48fb9ed191a0e2abc13655e9e588
firefly <schauss@tutanota.com>
2025-06-01 12:32:04 +0200

    changed frontend to simple test infrastructure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import { MantineProvider } from '@mantine/core'
-import { PartyButtonBox } from './PartyButtonBox';
+import './App.css';
 
 function App() {
-  const [score, setScore] = useState(0);
-  const [expression, setExpression] = useState("");
+  const handleAPItest = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/test", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ "message": "test" }) // test message
+      });
 
-  const handleButtonClick = () => {
-    setScore(score + 1);
-  }
+      const data = await response.json();
+      console.log("Received answer: ", data);
+    } catch (error) {
+      console.error("Error testing: ", error);
+    }
+  };
 
   return (
-    <MantineProvider >
-      <div style={{ textAlign: "center" }}>
-        <div style={{ width: "100%", alignItems: "center" }}>
-
-          <h1 style={{height: "10vh"}}>Titel des Spiels</h1>
-          <p style={{height: "20vh"}}>{expression}</p>
-
-          <p style={{height: "15vh"}}>Aktueller Score: {score}</p>
-          <PartyButtonBox handleButtonClick={handleButtonClick} setExpression={setExpression}/>
-        </div></div>
-    </MantineProvider>
-  )
+    <div>
+      <button onClick={handleAPItest}>Test API</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
