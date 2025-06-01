86eca6bebe69af7da21d6ae04062eb59b802f1ec
firefly <schauss@tutanota.com>
2025-06-01 13:06:40 +0200

    created testing buttons for color api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,15 @@ app.use(cors());
 app.use(express.json());
 
 let selectedColor = "black";
+let currentRoundInformation = {
+  "expression": "placeholder expression",
+  "name": "placeholder name",
+  "party": "placeholder party",
+  "difficulty": 0,
+  "date": "placeholder date",
+  "context": "placeholder context",
+  "link": "placeholder link"
+}
 
 // API test route
 app.post('/test', (req, res) => {
@@ -16,17 +25,25 @@ app.post('/test', (req, res) => {
 app.post('/getcolor', (req, res) => {
   let datetime = new Date();
   console.log("Received colour request at " + datetime);
-  res.json({"color": selectedColor});
+  res.json({ "color": selectedColor });
 })
 
 app.post('/setcolor', (req, res) => {
   let datetime = new Date();
-  console.log("Received colour modification at " + datetime);
   selectedColor = req.body.color;
-  res.json({"message": "ok"});
+  console.log("Received colour modification at " + datetime + " to color: " + selectedColor);
+  res.json({ "message": "ok" });
+})
+
+app.post('/getroundinformation', (req, res) => {
+  let datetime = new Date();
+  console.log("Received round information request at " + datetime);
+  res.json(currentRoundInformation);
 })
 
 // Setup
 app.listen(3000, () => {
   console.log(`Server is running on http://localhost:3000`);
 });
+
+// curl -X POST http://localhost:3000/getroundinformation -H-Type: apption: application/json" -d '{"message": "test"}'
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,9 +18,45 @@ function App() {
     }
   };
 
+  const handleGetColor = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/getcolor", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ "message": "test" }) // test message
+      });
+
+      const data = await response.json();
+      console.log("Received color: ", data);
+    } catch (error) {
+      console.error("Error testing: ", error);
+    }
+  };
+
+  const handleSetColor = async (setcolor: string) => {
+    try {
+      const response = await fetch("http://localhost:3000/setcolor", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ "color": setcolor })
+      });
+      console.log("success");
+    } catch (error) {
+      console.error("Error testing: ", error);
+    }
+  };
+
   return (
     <div>
       <button onClick={handleAPItest}>Test API</button>
+
+      <button onClick={handleGetColor}>Log color to console</button>
+      <button onClick={() => handleSetColor("white")}>Set color to white</button>
+      <button onClick={() => handleSetColor("black")}>Set color to black</button>
     </div>
   );
 }
