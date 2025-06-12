a3503a9f2414c94984392a1dd40b39ae1c057844
firefly <schauss@tutanota.com>
2025-06-12 19:30:03 +0200

    created Party enum field

--- backend/src/model.ts
+++ backend/src/model.ts
@@ -1,34 +1,37 @@
 interface ExpressionData {
-    expression: string;
-    name: string;
-    party: string;
-    difficulty: number;
-    date: number;
-    context?: string;
-    link: string;
+  expression: string;
+  name: string;
+  party: string;
+  difficulty: number;
+  date: number;
+  context?: string;
+  link: string;
 }
 
 let roundInformation: ExpressionData = {
-    "expression": "placeholder expression",
-    "name": "placeholder name",
-    "party": "placeholder party",
-    "difficulty": 0,
-    "date": 1000,
-    "context": "placeholder context",
-    "link": "placeholder link"
+  "expression": "placeholder expression",
+  "name": "placeholder name",
+  "party": "placeholder party",
+  "difficulty": 0,
+  "date": 1000,
+  "context": "placeholder context",
+  "link": "placeholder link"
 }
 
+type Party = "SPD" | "CDU" | "FW" | "GRU" | "AfD" | "LNK" | "BSW" | "FDP";
+
 let selectedColor = "black";
-let expressionArray:ExpressionData[] = [];
+let expressionArray: ExpressionData[] = [];
+let correctParty: Party = "SPD";
 
 // Getting current round information
 export const model_getRoundInformation = () => {
-    return roundInformation;
+  return roundInformation;
 };
 
 // Setting current round information
 export const model_setRoundInformation = (newRoundInformation: ExpressionData) => {
-    roundInformation = newRoundInformation;
+  roundInformation = newRoundInformation;
 };
 
 // Getting selected color
@@ -39,6 +42,16 @@ export const model_getSelectedColor = () => {
 // Setting selected color
 export const model_setSelectedColor = (newSelectedColor: string) => {
   selectedColor = newSelectedColor;
+}
+
+// Getting correct party
+export const model_getCorrectParty = () => {
+  return correctParty;
+}
+
+// Setting correct party
+export const model_setCorrectParty = (newCorrectParty: Party) => {
+  correctParty = newCorrectParty;
 }
 
 // Getting selected color
