9de8a18cd83f09c0a12f5aefdbd0adbe17c671df
firefly <schauss@tutanota.com>
2025-07-14 10:40:11 +0200

    cleanup for build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes";
-import NavBar from "./components/NavBar";
 import BackgroundMusic from "./components/audio/BackgroundMusic";
 import { SoundSettingsProvider } from "./components/audio/SoundSettings";
 
